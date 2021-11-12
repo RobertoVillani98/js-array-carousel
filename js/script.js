102c5f3d8e52fff3367a591f21e98f995702181c
RobertoVillani <robertovillani58@gmail.com>
2021-11-12 18:35:18 +0100

    add new img

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 const items = [
- "img/01.jpg",
- "img/02.jpg",
- "img/03.jpg",
- "img/04.jpg",
- "img/05.jpg",
+ "img/newyork.jpeg",
+ "img/abu.jpg",
+ "img/mykonos.jpeg",
+ "img/sharm.jpg",
+ "img/Barcellona.jpg",
 ];
 
-const title = ["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
+const title = [
+ "NewYork",
+ "Abu Dhabi",
+ "Mykonos",
+ "Sharm el-Sheikh",
+ "Barcellona",
+];
 
 const text = [
- "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
- "Lorem ipsum",
- "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
- "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
- "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+ "New York City, in breve New York, in italiano anche Nuova York è una città degli Stati Uniti d'America situata nello stato di New York.",
+ "Abu Dhabi, o più correttamente 'Abū Ẓabiyy, è la capitale degli Emirati Arabi Uniti e capitale dell'omonimo emirato.",
+ "Mykonos (Μύκονος) o Micono è un'isola greca delle Cicladi, situata nelle vicinanze di Tino, Siro, Paro e Nasso.",
+ "Sharm el-Sheikh è una località turistica egiziana posta nella parte meridionale della penisola del Sinai e bagnata dal mar Rosso.",
+ "Barcellona è una città della Spagna di 1 664 162 abitanti, capoluogo della Catalogna, una comunità autonoma della parte orientale dello Stato, oltre che dell'omonima provincia e della comarca del Barcelonès: soprannominata Ciutat Comtal o Ciudad Condal (Città dei Conti), è la seconda città della Spagna per numero di abitanti dopo la capitale Madrid.",
 ];
 
 let itemsContent = "";
